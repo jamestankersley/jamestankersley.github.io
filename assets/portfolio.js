b085861c8aaaa0067fa1a294e1da291be3278f23
James <jwtankersley10@gmail.com>
2020-04-16 19:10:36 -0500

    uploaded all files to old repo

---
+++ assets/portfolio.js
@@ -0,0 +1,32 @@
+const portfolio = [
+      {
+        title: "Project 1: Want 2 Go",
+        tagline: "An app to plan your outing",
+        url:" https://github.com/robertrunstrails71/Want2Go",
+        type: "coding"
+      },
+      {
+        title: "My Portfolio",
+        tagline: "James Tankersley's Portfolio",
+        url: "https://jamestankersley.github.io/",
+        type: "coding"
+      },
+      {
+        title: "Day Planner",
+        tagline: "A planner to schedule your day",
+        url: "https://github.com/jamestankersley/calendar",
+        type: "coding"
+      },
+      {
+        title: "Weather Dashbaord",
+        tagline: "A simple weather dashboard utilizing APIs",
+        url: "https://github.com/jamestankersley/weatherdashboard",
+        type: "coding"
+      },
+      {
+        title: "Password Generator",
+        tagline: "An application that generates a random password",
+        url: "https://github.com/jamestankersley/passwordgenerator" ,
+        type: "coding"
+      },
+  ];
